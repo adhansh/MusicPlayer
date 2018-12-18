1e11be0d1b672d05c5f8a753711e460473ca9864
한 석희 <han60719@han-ui-mac-mini.local>
2018-12-18 23:25:25 +0900

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by 한 석희 on 2018. 12. 18..
+//  Copyright © 2018년 한 석희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
